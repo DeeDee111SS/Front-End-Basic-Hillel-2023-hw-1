7d75cd01aac543f839885b2d98d3296fbe487913
Dmitrij Duvanov <you@example.com>
2023-03-01 00:54:57 +0200

    fixed

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
-let name = prompt('What`s your name?', '');
-alert('Hello, ' + name + '! How are you?');
+let name1 = prompt('What`s your name?', '');
+alert('Hello, ' + name1 + '! How are you?');
 
