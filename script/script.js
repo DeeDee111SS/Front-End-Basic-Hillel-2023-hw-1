454b036553007cfca2e35ea82311ae967c1ef9c0
Dmitrij Duvanov <you@example.com>
2023-03-01 21:19:25 +0200

    edited

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 let name1 = prompt('What`s your name?', '');
-alert('Hello, ' + name1 + '! How are you?');
+alert(`Hello, #${name1}! How are you?`);
 
+// if 'name' ->  const name: void
+// @deprecated
+// 'name' is deprecated.ts(6385)
+// lib.dom.d.ts(17693, 5): The declaration was marked as deprecated here.
